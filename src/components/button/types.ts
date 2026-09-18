81e65ab95c5a4719088a7c386d4457859b918f35
kanakana-chang <kanako07071212@gmail.com>
2026-09-18 14:21:09 +0900

    Revert "ボタンコンポーネントの作成"
    This reverts commit bc9631c9bafc0c4870286ca0b324dfce5326b2ed.

--- src/components/button/types.ts
+++
@@ -1,8 +0,0 @@
-import type { ButtonHTMLAttributes } from "react";
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  label: string;
-  onClick?: () => void;
-  variant?: "primary" | "secondary" | "success" | "danger";
-  size?: "medium" | "large";
-}
