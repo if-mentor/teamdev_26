dd65ec5cc2d3cfa1b77df0a3f2b66b5ba6270087
Ayaka <peapocoron2353@gmail.com>
2026-09-19 13:54:37 +0900

    fix: Inputのfont-family削除とclassName記述を修正

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,16 +2,28 @@ import styles from "./styles.module.css";
 import type { InputProps } from "./type";
 
 const Input = ({ label, error, variantSize = "medium", className = "", ...props }: InputProps) => {
-  const inputClassName = [styles.input, styles[variantSize], error ? styles.inputError : "", className]
-    .filter(Boolean)
-    .join(" ");
-
+  <input
+    {...props}
+    className={`
+    ${styles.input}
+    ${styles[variantSize]}
+    ${error ? styles.inputError : ""}
+    ${className}
+  `}
+  />;
   return (
     <label className={styles.wrapper}>
       {label && <span className={styles.label}>{label}</span>}
 
-      <input {...props} className={inputClassName} />
-
+      <input
+        {...props}
+        className={`
+    ${styles.input}
+    ${styles[variantSize]}
+    ${error ? styles.inputError : ""}
+    ${className}
+  `}
+      />
       {error && <span className={styles.errorMessage}>{error}</span>}
     </label>
   );
