ebda818fdd569ea63ab7b1f2891eb48ae5485ceb
ryu0929 <yourai_sakuyo@yahoo.co.jp>
2026-09-20 15:53:01 +0900

    fix: #4 page.tsxを元に戻す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
-// import styles from "./styles.module.css";
-
-import SelectBox from "@/components/SelectBox/SelectBox";
-import type { Option } from "@/components/SelectBox/type";
-
-const options: Option[] = [
-  {
-    id: 1,
-    value: "東京",
-  },
-  {
-    id: 2,
-    value: "大阪",
-  },
-  {
-    id: 3,
-    value: "福岡",
-  },
-];
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
-    // <div>
-    //   <h1 className={styles.title}>Hello Teamdev!!</h1>
-    // </div>
-
-    <SelectBox options={options} label="カテゴリー" placeholder="カテゴリ選択" />
+    <div>
+      <h1 className={styles.title}>Hello Teamdev!!</h1>
+    </div>
   );
 }
