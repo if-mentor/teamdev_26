ca393ffe7f9369994fd145b1b3b6cd840fd614db
ayaka-tsu <peapocoron2353@gmail.com>
2026-09-21 01:42:59 +0900

    Inputコンポーネントを作成 (#8)
    * feat: Inputコンポーネントを作成
    
    * fix: Inputのfont-family削除とclassName記述を修正
    
    * fix: Inputのインデントを修正

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+import type { InputProps } from "./type";
+
+const Input = ({ label, error, variantSize = "medium", className = "", ...props }: InputProps) => {
+  <input
+    {...props}
+    className={`
+      ${styles.input}
+      ${styles[variantSize]}
+      ${error ? styles.inputError : ""}
+      ${className}
+    `}
+  />;
+  return (
+    <label className={styles.wrapper}>
+      {label && <span className={styles.label}>{label}</span>}
+      <input
+        {...props}
+        className={`
+          ${styles.input}
+          ${styles[variantSize]}
+          ${error ? styles.inputError : ""}
+          ${className}
+        `}
+      />
+      {error && <span className={styles.errorMessage}>{error}</span>}
+    </label>
+  );
+};
+
+export default Input;
