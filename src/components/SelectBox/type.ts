ec9ebbdf73019a80126455d230bfe778eeaaf0a3
ryu0929 <yourai_sakuyo@yahoo.co.jp>
2026-09-20 16:01:04 +0900

    fix: #4 type.ts修正

--- src/components/SelectBox/type.ts
+++ src/components/SelectBox/type.ts
@@ -1,12 +1,12 @@
+import type { SelectHTMLAttributes } from "react";
+
 export interface Option {
   id: number;
   value: string;
 }
 
-export interface SelectBoxProps {
+export interface SelectBoxProps extends SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
   options: Option[];
   placeholder?: string;
-  disabled?: boolean;
-  onChange?: (value: string) => void;
 }
