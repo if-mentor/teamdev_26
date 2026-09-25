c6d4e7ed663d44e3cdfa763ce3ab677429cb42da
kanakana-chang <kanako07071212@gmail.com>
2026-09-25 11:15:29 +0900

    fix: ClassNameの修正

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,10 +2,18 @@ import { ButtonProps } from "./type";
 import styles from "./button.module.css";
 
 const Button = ({ label, type, onClick, disabled, variant = "primary", size = "medium", ...props }: ButtonProps) => {
-  const buttonClassName = [styles.button, styles[variant], styles[size]].join(" ");
-
   return (
-    <button type={type} className={buttonClassName} onClick={onClick} disabled={disabled} {...props}>
+    <button
+      type={type}
+      className={`
+  ${styles.button}
+  ${styles[variant]}
+  ${styles[size]}
+`}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
       {label}
     </button>
   );
